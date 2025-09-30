9b0226b7d50765d454b8d5d1a0f24277f8595e65
Ryan0050 <lawyran0511@gmail.com>
2025-09-30 10:22:24 +0700

    push

---
+++ flask-ai-backend/app.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, jsonify
+from tensorflow.keras.models import load_model
+import cv2
+import numpy as np
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+# Load the pre-trained model
+model = load_model('models/pneumonia_model.keras')
+labels = ['PNEUMONIA', 'NORMAL']
+img_size = 200  # Input size for the model
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if 'image' not in request.files:
+        return jsonify({'error': 'No files uploaded'}), 400
+
+    images = request.files.getlist('image')
+    predictions = []
+
+    try:
+        for img_file in images:
+            img_arr = cv2.imdecode(np.frombuffer(img_file.read(), np.uint8), cv2.IMREAD_GRAYSCALE)
+            resized_arr = cv2.resize(img_arr, (img_size, img_size)).reshape(1, img_size, img_size, 1)
+
+            normalized_img = resized_arr / 255.0
+
+            prediction = model.predict(normalized_img)
+            print(prediction)
+            confidence = float(prediction[0][0])
+
+            if confidence > 0.80:
+                label = 'NORMAL'
+            else:
+                label = 'PNEUMONIA'
+
+            predictions.append({
+                'result': label
+            })
+
+        return jsonify(predictions)
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Components/Home'; // Create a separate Home component
+import About from './Components/About'; // Move your About content to a separate file
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
   );
 }
 
